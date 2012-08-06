27674d1b4d296678604b32315593d359ddb16598
Matt DeBoard <matt.deboard@gmail.com>
2012-08-06 07:59:42 -0400

    Change 'proxy-meta' fn to macro, and add support to generate a PdfWriter instance.

--- src/clj-itext/core.clj
+++ src/clj-itext/core.clj
@@ -1,7 +1,7 @@
 (ns render-pdf.core
   (:use [clojure.string :only (split join)])
   (:import [com.itextpdf.text Document]
-           [com.itextpdf.text.pdf PdfCopy PdfReader]))
+           [com.itextpdf.text.pdf PdfCopy PdfReader PdfWriter]))
 
 
 (defn- copy-page
@@ -11,7 +11,7 @@ returns it.
 This is icky and gross but an unfortunate side effect of the really goofy
 iText API.
 "
-  [document outstream]
+  [^Document document outstream]
   (let [pdfcopy (new PdfCopy document outstream)]
     (. document open)
     pdfcopy))
@@ -22,14 +22,14 @@ iText API.
   (let [fn (str filename "-" pagenum "." ext)]
     (join "/" [path fn])))
 
-(defn- write-pdf [pagenum reader writer]
+(defn- write-pdf [pagenum ^PdfReader reader writer]
   (let [page (. writer getImportedPage reader pagenum)]
     (. writer addPage page)
     (. writer close)))
         
 (defn burst
   "'Burst' a single PDF with n pages into n separate PDF files."
-  [reader]
+  [^PdfReader reader]
   (let [doc (new Document (. reader getPageSizeWithRotation 1))
         spl (split (:filename (meta reader)) #"(\.|/)")
         ct (count spl)
@@ -40,22 +40,41 @@ iText API.
       (with-open [os (clojure.java.io/output-stream (outpath pn))]
         (->> os (copy-page doc) (write-pdf pn reader))))))
 
-(defn proxy-meta
-  "Returns an anonymous class instance, constructed with the file on the path
-indicated by `infile', annotated with metadata indicating the filename used to
-construct the instance. Optional `moremeta' is a single map containing any
-additional metadata to add to the instance."
-  [infile & [moremeta]]
-  (let [metadata (merge {:filename infile} moremeta)]
-    (proxy [PdfReader clojure.lang.IObj] [infile]
-      (withMeta [newmeta] (proxy-meta infile newmeta))
-      (meta [] metadata))))
+(defmacro proxy-meta
+  "This macro emits a function that can then be called with the appropriate
+args to proxy a given class, extending it to implement the `IObj' interface."
+  [class]
+  `(fn make#
+     [infile# & [moremeta#]]
+     (let [metadata# (merge {:filename infile#} moremeta#)]
+       (proxy [~class clojure.lang.IObj] [infile#]
+         (withMeta [newmeta#] (make# infile# newmeta#))
+         (meta [] metadata#)))))
 
 (defn pdf<-
   "Returns PdfReader instance. The returned instance has metadata attached
 to access various data about the PDF, such as the filename of the PDF, the
 number of pages, and so forth."
   [infile]
-  (let [rdr (proxy-meta infile)]
+  (let [rdr ((proxy-meta PdfReader) infile)]
     (vary-meta rdr assoc :pagecount (. rdr getNumberOfPages))))
 
+(defn ->pdf
+  "Returns a PdfWriter instance."
+  [outfile]
+  (with-open [w (clojure.java.io/output-stream outfile)]
+    (let [doc (doto (new Document) (.open))
+          wrtr (PdfWriter/getInstance doc w)]
+      (. wrtr open)
+      wrtr)))
+
+;; (macroexpand-1
+;;  `(let [w (->pdf "resources/foo1.pdf")]
+;;     (. w getDirectContent)
+;;     (. w close)))
+
+;; (let [[d w] (->pdf "resources/foo1.pdf")]
+;;   (. w getDirectContent)
+;;   (. d isOpen))
+;; (with-pdf-open [w (->pdf "resources/foo1.pdf")]
+;;   (. w getDirectContent))
