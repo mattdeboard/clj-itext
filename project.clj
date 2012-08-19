4f77c836e7c89ef8a86f657a294967a053178edf
Matt DeBoard <matt.deboard@gmail.com>
2012-08-18 21:48:56 -0400

    Bump project version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-itext "0.1.0-SNAPSHOT"
+(defproject clj-itext "0.2.0"
   :description "Clojure API for iText Java bindings."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
