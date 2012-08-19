68710cc7143fe64a111a18c99060ffc3dbe1119a
Matt DeBoard <matt.deboard@gmail.com>
2012-08-18 21:48:05 -0400

    Remove unused dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.1.2"]
-                 [org.clojure/data.zip "0.1.1"]
                  [com.itextpdf/itextpdf "5.1.3"]
                  [com.itextpdf.tool/xmlworker "1.1.1"]])
