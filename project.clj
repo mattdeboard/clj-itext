0a2e123d82b336f9424346b042f96c3649dfa934
Matt DeBoard <matt.deboard@gmail.com>
2012-09-04 21:35:39 -0400

    Add docstring to '(proxy-meta)' to better explain why it's a macro instead of a function.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Clojure API for iText Java bindings."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/mattdeboard/clj-itext"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.itextpdf/itextpdf "5.1.3"]
                  [com.itextpdf.tool/xmlworker "1.1.1"]])
--- src/clj_itext/core.clj
+++ src/clj_itext/core.clj
@@ -43,7 +43,13 @@ iText API.
 
 (defmacro proxy-meta
   "This macro emits a function that can then be called with the appropriate
-args to proxy a given class, extending it to implement the `IObj' interface."
+args to proxy a given class, extending it to implement the `IObj' interface.
+
+The reason a macro is used instead of a function is because the first arg to
+`(proxy)' is a collection, the first argument `(proxy)' is itself a macro. In
+practical terms, this means that the args for `(proxy)' are evaluated at compile
+ time, so they must be set then.
+"
   [class]
   `(fn make#
      [infile# & [moremeta#]]
